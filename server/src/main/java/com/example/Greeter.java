e6ef2ca9ee8300f4c082ea500f727403ef4fd0ed
Jv <jv.nihilistik@gmail.com>
2018-12-02 11:29:57 +0100

    Fix FinalParametersCheck

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /*
+  * @param someone person to greet
+  * @return String person greetings
+  */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
